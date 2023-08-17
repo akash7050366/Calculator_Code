5eef45b000563676680d7cc2b382f72e2424af93
Akash <akash7050366@gmail.com>
2023-08-17 15:20:25 +0530

    complet program of html css and javaScript

---
+++ cal.js
@@ -0,0 +1,24 @@
+let input =document.getElementById('ip');
+let button = document.querySelectorAll('button');
+let string="";
+let arr=Array.from(button);
+arr.forEach(button=>{
+    button.addEventListener('click',(e)=>{
+        if(e.target.innerHtml=='='){
+            string=eval(string);
+            input.value=string;
+        }
+        else if(e.target.innerHtml=='AC'){
+            string="";
+            input.value=string;
+        }
+       else if(e.target.innerHtml=='C'){
+        string=string.substring(0,string.length-1);
+        input.value=string;
+       }
+       else{
+        string+=e.target.innerHtml;
+        input.value=string;
+       }
+    })  
+})
